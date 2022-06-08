647755beab470fd8e9e3224719aca80c8c4ab373
leeyoungchun <dldydcns123@gmail.com>
2022-06-08 23:15:10 +0900

    JDBC를 직접 사용해서 member 수정/삭제

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -66,6 +66,46 @@ public class MemberRepositoryV0 {
     }
 
 
+    public void update(String memberId, int money) throws SQLException {
+        String sql = "update member set money=? where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+            int resultSize = pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        }finally {
+            close(con,pstmt, null);
+        }
+    }
+
+
+    public void delete(String memberId) throws SQLException {
+        String sql = "delete from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            int resultSize = pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        }finally {
+            close(con,pstmt, null);
+        }
+    }
+
     private void close(Connection con, Statement stmt, ResultSet rs){ //Exception 터지면 con.close가 실행안된다. 때문에 지금처럼 구현해야함
         if(rs != null){
             try {
