f2bd5e64762a95ac46bf9d6f750ba0e1367fc431
leeyoungchun <dldydcns123@gmail.com>
2022-06-08 22:52:14 +0900

    JDBC를 직접 사용해서 member 조회

--- src/main/java/hello/jdbc/domain/Member.java
+++ src/main/java/hello/jdbc/domain/Member.java
@@ -1,8 +1,13 @@
 package hello.jdbc.domain;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+import java.util.Objects;
+
+@Getter
+@Setter
 public class Member {
 
     private String memberId;
@@ -14,5 +19,18 @@ public class Member {
     public Member(String memberId, int money) {
         this.memberId = memberId;
         this.money = money;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return getMoney() == member.getMoney() && Objects.equals(getMemberId(), member.getMemberId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getMemberId(), getMoney());
     }
 }
